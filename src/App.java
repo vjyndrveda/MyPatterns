e8d8aa1d5be217d4066655a4fdc96b5a6a42925e
Vijayendra Chauhan <vjyndrveda@gmail.com>
2023-01-14 21:11:59 +0530

    ff

--- src/App.java
+++ src/App.java
@@ -1,7 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        pattern15(5);
+        pattern16(5);
+    }
+
+    static void pattern16(int n) {
+        for (int row = 0; row < n; row++) {
+
+            for (int col = 1; col <= 2 * n - 2 * row + 1; col++) {
+                System.out.print(" ");
+            }
+            int number = 1;
+            for (int col = 0; col <= row; col++) {
+                System.out.print("   " + number);
+                number = number * (row - col) / (col + 1);
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void pattern30(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            for (int col = 1; col <= n - row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col);
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
     }
 
     static void pattern15(int n) {
